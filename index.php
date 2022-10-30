917c9d3026a2d464d0c5c570e87a154d7110dd43
Masruril Akmal Yazid <akmalyazid76@gmail.com>
2022-10-30 16:52:52 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+
+echo "Hello World <br>" ;
+
+$nama = "Yazid" ;
+
+$umur = 21 ;
+
+echo "nama saya $nama saya berusia $umur tahun <br>" ;
+
+$namaadik = "ece" ;
+$umuradik = 7 ;
+
+echo "nama adik saya <strong>$namaadik</strong>, adik saya berusia $umuradik <br>" ;
+
+$selisihumur = $umur - $umuradik ;
+
+echo "selisih usia saya dan adik saya adalah $selisihumur tahun" ;
+?>
