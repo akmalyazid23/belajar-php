069bbdef63c89751092a92f324757eaabd09b14d
Masruril Akmal Yazid <akmalyazid76@gmail.com>
2022-12-21 21:29:57 +0700

    php_crud

--- index.php
+++ index.php
@@ -1,19 +1,56 @@
 <?php
+// 1.membuat koneksi mysql
+$con = mysqli_connect("localhost","root","","fakultas");
 
-echo "Hello World <br>" ;
+//2. cek koneksi
+if (mysqli_connect_errno()){
+    echo "koneksi gagal" . mysqli_connect_error();
+} else{
+    echo "koneksi berhasil";
+}
 
-$nama = "Yazid" ;
+//3. membaca data dari data table mysql
+$query = "SELECT * FROM mahasiswa";
 
-$umur = 21 ;
+//4. menampilkan data
+$result = mysqli_query($con,$query);
+$mahasiswa = [];
+if ($result) {
+    //tampilkan data satu-persatu
+    while($row = mysqli_fetch_assoc($result)){
+        $mahasiswa[] = $row; 
+    }
+    mysqli_free_result($result);
+}
 
-echo "nama saya $nama saya berusia $umur tahun <br>" ;
-
-$namaadik = "ece" ;
-$umuradik = 7 ;
-
-echo "nama adik saya <strong>$namaadik</strong>, adik saya berusia $umuradik <br>" ;
-
-$selisihumur = $umur - $umuradik ;
-
-echo "selisih usia saya dan adik saya adalah $selisihumur tahun" ;
+// tutup koneksi mysql
+mysqli_close($con);
+  
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Data Mahasiswa</title>
+</head>
+<body> 
+    <h1>Data Mahasiswa</h1>
+    <table border="1" style="width:100%;">
+    <tr>
+        <th>NIM</th>
+        <th>Nama</th>
+    </tr>
+    <?php foreach($mahasiswa as $value): ?>
+    <tr> 
+        <td> <?php echo $value ["nim"]; ?></td>
+        <td> <?php echo $value ["nama"]; ?></td>
+    </tr>
+    <?php endforeach; ?>
+
+    </table>
+    
+</body>
+</html>
